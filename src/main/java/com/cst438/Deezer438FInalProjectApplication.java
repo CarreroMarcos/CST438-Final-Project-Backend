16ec0e6534ad2314f0fc73818f1fd20773d8182e
Marcos Carrero <carr.marcos131@gmail.com>
2023-11-07 11:45:00 -0800

    initial commit

---
+++ src/main/java/com/cst438/Deezer438FInalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deezer438FInalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deezer438FInalProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+create table user_table (
+	user_id int NOT NULL AUTO_INCREMENT,  
+	alias varchar(25) unique, 
+	email varchar(25) unique,
+	password varchar(100) NOT NULL, 
+	role varchar(25),
+	primary key (user_id)
+);
+
+create table song(
+	deezer_id int NOT NULL primary key,
+	title varchar(45) NOT NULL,
+	duration int,
+	artist varchar(45),
+	cover_art varchar(150)
+);
+
+create table user_library(
+	library_id int NOT NULL AUTO_INCREMENT,
+	user_id int NOT NULL,
+	deezer_id int NOT NULL,
+	primary key (library_id),
+	FOREIGN KEY (user_id) REFERENCES user_table (user_id) on delete cascade,
+	FOREIGN KEY (deezer_id) REFERENCES song (deezer_id)
+);
